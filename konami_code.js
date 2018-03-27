be5c02cce68532c7fa2282eea1a9767135137555
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-27 04:43:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,6 +3,12 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index=0;
   document.body.addEventListener('keydown', function(e) {
-    const
-  })
+    const ky = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++
+    }
+     if (index === code.length) {alert("Good work, Narwhal!")
+     index=0
+  }
+})
 }
